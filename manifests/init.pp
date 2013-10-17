503b5d02b63f3a82c869307a1442e88d5152cecb
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-10-17 19:36:07 +0200

    Fix provider

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 class geppetto {
 
   package { 'Geppetto-3.2.0':
-    provider => 'zip',
+    provider => 'compressed_app',
     source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.0.0-R201310140657.zip'
   }
 
--- spec/classes/geppetto_spec.rb
+++ spec/classes/geppetto_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'geppetto' do
   it do
     should contain_package('Geppetto-3.2.0').with({
-      :provider => 'zip',
+      :provider => 'compressed_app',
       :source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.0.0-R201310140657.zip'
     })
   end
