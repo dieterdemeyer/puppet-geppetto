122d4af7d3da2bde40babcfed0361d7092a8a878
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-11-01 20:07:38 +0100

    Fix package resource name

--- spec/classes/geppetto_spec.rb
+++ spec/classes/geppetto_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'geppetto' do
   it do
-    should contain_package('Geppetto-3.2.0').with({
+    should contain_package('Geppetto-4.0.0').with({
       :provider => 'compressed_app',
       :source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.0.0-R201310140657.zip'
     })
