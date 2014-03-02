045975ec28e54064d80101d0df179bd2d87e23a3
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-03-02 16:35:33 +0100

    Update to latest version

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,9 @@
 #
 class geppetto {
 
-  package { 'Geppetto-4.0.0':
+  package { 'Geppetto-4.1.0':
     provider => 'compressed_app',
-    source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.0.0-R201310140657.zip'
+    source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.1.0-R201402150632.zip'
   }
 
 }
--- spec/classes/geppetto_spec.rb
+++ spec/classes/geppetto_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
 describe 'geppetto' do
-  it do
-    should contain_package('Geppetto-4.0.0').with({
-      :provider => 'compressed_app',
-      :source   => 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.0.0-R201310140657.zip'
-    })
-  end
+
+  it { should contain_class('geppetto') }
+  it { should contain_package("Geppetto-4.1.0").with_provider('compressed_app') }
+  it { should contain_package("Geppetto-4.1.0").with_source("https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.1.0-R201402150632.zip") }
+
 end
